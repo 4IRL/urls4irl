31ecd7b79ebccaca07936b87b393c2c6799d7172
Giovanni Propersi <giovannigp@gmail.com>
2022-12-10 17:13:12 -0600

    Updated to allow environmental variable changing from test to development and vice versa

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ from os import environ
 from dotenv import load_dotenv
 from urls4irl.config import TestingConfig
 
-load_dotenv()
+load_dotenv(override=True)
 
 if environ.get('TESTING').lower() == 'true':
+    print("In testing")
     app = create_app(TestingConfig, True)
 else:
+    print("Not in testing")
     app = create_app()
 
 if __name__ == "__main__":
