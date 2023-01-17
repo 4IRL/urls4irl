23263e07ad904d28d1b13b756c841961c1aabe99
Giovanni Propersi <giovannigp@gmail.com>
2023-01-17 10:38:53 -0600

    Start of Update for UTub name functions and tests

---
+++ tests/functional/test_update_utub_name_route.py
@@ -0,0 +1,8 @@
+# TODO - Test valid name update by creator
+# TODO - Test same name update
+# TODO - Test empty name update
+# TODO - Test name update by member
+# TODO - Test name update by creator of another UTub
+# TODO - Test name update to invalid UTub
+# TODO - Test name update without name form field
+# TODO - Test name update without csrf token
--- urls4irl/utubs/routes.py
+++ urls4irl/utubs/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 from flask_login import current_user, login_required
 from urls4irl import db
 from urls4irl.models import Utub, Utub_Users
-from urls4irl.utubs.forms import UTubForm, UTubDescriptionForm
+from urls4irl.utubs.forms import UTubForm, UTubDescriptionForm, UTubNewNameForm
 
 utubs = Blueprint('utubs', __name__)
 
@@ -86,6 +86,65 @@ def delete_utub(utub_id: int):
             "UTub_description" : f"{utub.utub_description}",
         }), 200
 
+@utubs.route('/utub/edit_name/<int:utub_id>', methods=["POST"])
+@login_required
+def update_utub_name(utub_id: int):
+    """
+    Creator wants to update their UTub name.
+    Name limit is 30 characters.
+    Form data required to be sent from the frontend with a parameter "name".
+
+    Input is required and the new name cannot be empty. Members cannot update UTub names. Creators
+    of other UTubs cannot update another UTub's name. The "name" field must be included in the form.
+    
+    On POST:
+        The new name is saved to the database for that UTub.
+
+    Args:
+        utub_id (int): The ID of the UTub that will have its description updated
+    """
+    current_utub = Utub.query.get_or_404(utub_id)
+
+    if int(current_user.get_id()) != current_utub.created_by.id:
+        return jsonify({
+            "Status" : "Failure",
+            "Message": "You do not have permission to edit this UTub's name",
+            "Error_code": 1,
+        }), 403
+
+    current_utub_name = current_utub.name
+
+    utub_name_form = UTubNewNameForm()
+
+    if utub_name_form.validate_on_submit():
+        new_utub_name = utub_name_form.name.data
+
+        if new_utub_name != current_utub_name:
+            current_utub.name = new_utub_name
+            db.session.commit()
+
+        return jsonify({
+            "Status": "Success",
+            "UTub_ID": current_utub.id,
+            "UTub_name": current_utub.name,
+            "UTub_description": current_utub.utub_description,
+        }), 200
+
+    # Invalid form errors
+    if utub_name_form.errors is not None:
+        return jsonify({
+            "Status": "Failure",
+            "Message": "Invalid form",
+            "Error_code": 3,
+            "Errors": utub_name_form.errors
+        }), 404
+
+    return jsonify({
+        "Status" : "Failure",
+        "Message" : "Unable to modify this UTub's name",
+        "Error_code": 4
+    }), 404
+
 @utubs.route('/utub/edit_description/<int:utub_id>', methods=["POST"])
 @login_required
 def update_utub_desc(utub_id: int):
@@ -150,5 +209,6 @@ def update_utub_desc(utub_id: int):
 
     return jsonify({
         "Status" : "Failure",
-        "Message" : "Unable to modify this UTub's description"
+        "Message" : "Unable to modify this UTub's description",
+        "Error_code": 4
     }), 404
