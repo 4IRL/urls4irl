9bd730a4721fdc0cd7657a6933faa3e371435faa
Giovanni Propersi <giovannigp@gmail.com>
2024-11-12 08:53:44 -0800

    Test for all possible kinds of session interface engines

--- tests/integration/cli/test_login_user_via_cli.py
+++ tests/integration/cli/test_login_user_via_cli.py
@@ -1,4 +1,8 @@
+from typing import Tuple
+
 from click.testing import Result
+from flask import Flask
+from flask.testing import FlaskCliRunner
 import pytest
 
 from src.mocks.mock_constants import TEST_USER_COUNT
@@ -7,7 +11,7 @@ from src.mocks.mock_options import USER_ID_INVALID_TO_LOGIN_WITH
 pytestmark = pytest.mark.cli
 
 
-def test_login_valid_user_via_cli(runner):
+def test_login_valid_user_via_cli(runner: Tuple[Flask, FlaskCliRunner]):
     app, cli_runner = runner
 
     for user_id in range(1, TEST_USER_COUNT + 1):
@@ -15,11 +19,10 @@ def test_login_valid_user_via_cli(runner):
         user_session_id = result.output.strip()
 
         with app.app_context():
-            assert (
-                b"session:" + user_session_id.encode()
-                in app.session_interface.client.keys("*")
-            )
+            store_id = f"session:{user_session_id}"
+            assert app.session_interface._retrieve_session_data(store_id) is not None
             assert result.exit_code == 0
+            app.session_interface._delete_session(store_id)
 
 
 def test_login_invalid_user_via_cli(runner):
