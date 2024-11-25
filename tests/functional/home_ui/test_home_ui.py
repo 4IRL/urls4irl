311d77bbfdea2b6a1eda46808ce1dedc9bcce3a4
Rehan Kaluarachchi <rehankalu@gmail.com>
2024-11-24 23:06:16 -0500

    refresh logo

--- tests/functional/home_ui/test_home_ui.py
+++ tests/functional/home_ui/test_home_ui.py
@@ -64,6 +64,9 @@ def test_refresh_logo(browser: WebDriver, create_test_utubs):
     # Find submit button to login
     wait_then_click_element(browser, SPL.BUTTON_SUBMIT)
 
+    # Find submit button to login
+    wait_then_click_element(browser, SPL.BUTTON_SUBMIT)
+
     wait_then_click_element(browser, MPL.U4I_LOGO)
 
     assert_login(browser)
