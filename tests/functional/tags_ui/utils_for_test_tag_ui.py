8cd7259f474cb00bfd0a5ad2e279aadd9377e536
Rehan Kaluarachchi <rehankalu@gmail.com>
2025-01-05 21:49:06 -0500

    Tests updated to include new button. I want to revise the test to better evaluate unfiltered URLs (rather than merely compare visible count). And I want to fix unselectAll visibility after Tag Filter Creation in Deck

--- tests/functional/tags_ui/utils_for_test_tag_ui.py
+++ tests/functional/tags_ui/utils_for_test_tag_ui.py
@@ -173,3 +173,17 @@ def verify_new_utub_tag_created(
         By.CSS_SELECTOR, HPL.TAG_FILTERS + " span"
     )
     assert new_tag_str in [tag.text for tag in utub_tag_spans]
+
+
+def assert_clear_all_tag_filters_disabled(browser: WebDriver):
+
+    unselect_all_selector = browser.find_element(
+        By.CSS_SELECTOR, HPL.SELECTOR_UNSELECT_ALL
+    )
+
+    # Assert Unselect All filter is disabled
+    assert "disabled" in unselect_all_selector.get_attribute("class").split()
+
+    unselect_all_button = browser.find_element(By.CSS_SELECTOR, HPL.BUTTON_UNSELECT_ALL)
+
+    assert not unselect_all_button.is_displayed()
