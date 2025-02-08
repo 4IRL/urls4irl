4175e5804812b1ab0f30246ef783d984569f754b
Giovanni Propersi <80492028+GPropersi@users.noreply.github.com>
2025-02-07 22:23:52 -0600

    Tests/frontend/sad path url update delete access (#381)
    * Sad path tests for update url string or title
    
    * Add sad path url tests for deleting URL
    
    * Fix missing import..
    
    * Adding some robustness to animation UI testing...

--- tests/functional/urls_ui/test_update_url_ui.py
+++ tests/functional/urls_ui/test_update_url_ui.py
@@ -1,10 +1,7 @@
-# Standard library
 import random
-from time import sleep
 from typing import Tuple
 from urllib.parse import urlsplit
 
-# External libraries
 from flask import Flask
 from flask.testing import FlaskCliRunner
 import pytest
@@ -13,24 +10,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 
-# Internal libraries
 from src.cli.mock_constants import (
     MOCK_URL_STRINGS,
 )
+from src.models.users import Users
+from src.models.utub_urls import Utub_Urls
+from src.models.utubs import Utubs
+from src.utils.constants import URL_CONSTANTS
 from src.utils.strings.ui_testing_strs import UI_TEST_STRINGS as UTS
+from src.utils.strings.url_strs import URL_FAILURE
 from tests.functional.locators import HomePageLocators as HPL
 from tests.functional.utils_for_test import (
     add_mock_urls,
+    assert_login_with_username,
+    assert_visited_403_on_invalid_csrf_and_reload,
     get_selected_url,
+    invalidate_csrf_token_on_page,
     login_user_select_utub_by_name_and_url_by_string,
     login_user_select_utub_by_name_and_url_by_title,
-    login_utub_url,
     verify_update_url_state_is_hidden,
     verify_update_url_state_is_shown,
+    wait_then_click_element,
     wait_then_get_element,
     wait_until_hidden,
+    wait_until_in_focus,
+    wait_until_visible_css_selector,
 )
 from tests.functional.urls_ui.utils_for_test_url_ui import (
+    add_invalid_url_header_for_ui_test,
     update_url_title,
     update_url_string,
 )
@@ -79,7 +86,7 @@ def test_update_url_string_submit_btn(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     # Extract URL string from updated URL row
-    url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    url_row_string_display = url_row_string_elem.text
     url_row_data_attrib = url_row_string_elem.get_attribute("data-url")
 
     assert url_row_data_attrib == url_row_string_display
@@ -140,7 +147,7 @@ def test_update_url_string_press_enter_key(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     # Extract URL string from updated URL row
-    url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    url_row_string_display = url_row_string_elem.text
     url_row_data_attrib = url_row_string_elem.get_attribute("data-url")
 
     assert url_row_data_attrib == url_row_string_display
@@ -193,7 +200,7 @@ def test_update_url_string_big_cancel_btn(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     init_url_row_data = url_row_string_elem.get_attribute("data-url")
-    init_url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    init_url_row_string_display = url_row_string_elem.text
 
     url_row.find_element(By.CSS_SELECTOR, HPL.BUTTON_URL_STRING_UPDATE).click()
     verify_update_url_state_is_shown(url_row)
@@ -209,7 +216,7 @@ def test_update_url_string_big_cancel_btn(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     # Extract URL string from updated URL row
-    url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    url_row_string_display = url_row_string_elem.text
     url_row_data_attrib = url_row_string_elem.get_attribute("data-url")
 
     assert url_row_data_attrib == init_url_row_data
@@ -256,7 +263,7 @@ def test_update_url_string_cancel_btn(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     init_url_row_data = url_row_string_elem.get_attribute("data-url")
-    init_url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    init_url_row_string_display = url_row_string_elem.text
     url_row.find_element(By.CSS_SELECTOR, HPL.BUTTON_URL_STRING_UPDATE).click()
     verify_update_url_state_is_shown(url_row)
 
@@ -271,7 +278,7 @@ def test_update_url_string_cancel_btn(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     # Extract URL string from updated URL row
-    url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    url_row_string_display = url_row_string_elem.text
     url_row_data_attrib = url_row_string_elem.get_attribute("data-url")
 
     assert url_row_data_attrib == init_url_row_data
@@ -318,12 +325,14 @@ def test_update_url_string_escape_key(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     init_url_row_data = url_row_string_elem.get_attribute("data-url")
-    init_url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    init_url_row_string_display = url_row_string_elem.text
     url_row.find_element(By.CSS_SELECTOR, HPL.BUTTON_URL_STRING_UPDATE).click()
     verify_update_url_state_is_shown(url_row)
 
     # Sleep required to allow the element to come into focus
-    sleep(2)
+    wait_until_in_focus(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.INPUT_URL_STRING_UPDATE}"
+    )
     browser.switch_to.active_element.send_keys(Keys.ESCAPE)
     wait_until_hidden(browser, HPL.UPDATE_URL_STRING_WRAP)
     verify_update_url_state_is_hidden(url_row)
@@ -331,7 +340,7 @@ def test_update_url_string_escape_key(
     url_row_string_elem = url_row.find_element(By.CSS_SELECTOR, HPL.URL_STRING_READ)
 
     # Extract URL string from updated URL row
-    url_row_string_display = url_row_string_elem.get_attribute("innerText")
+    url_row_string_display = url_row_string_elem.text
     url_row_data_attrib = url_row_string_elem.get_attribute("data-url")
 
     assert url_row_data_attrib == init_url_row_data
@@ -381,9 +390,7 @@ def test_update_url_title_submit_btn(
     assert url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).is_displayed()
 
     # Extract URL string from updated URL row
-    url_row_title = url_row.find_element(
-        By.CSS_SELECTOR, HPL.URL_TITLE_READ
-    ).get_attribute("innerText")
+    url_row_title = url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).text
 
     assert url_title == url_row_title
 
@@ -424,9 +431,7 @@ def test_update_url_title_submit_enter_key(
     assert url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).is_displayed()
 
     # Extract URL string from updated URL row
-    url_row_title = url_row.find_element(
-        By.CSS_SELECTOR, HPL.URL_TITLE_READ
-    ).get_attribute("innerText")
+    url_row_title = url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).text
 
     assert url_title == url_row_title
 
@@ -457,9 +462,7 @@ def test_update_url_title_cancel_click_btn(
     url_row = get_selected_url(browser)
 
     # Extract URL string from updated URL row
-    init_url_row_title = url_row.find_element(
-        By.CSS_SELECTOR, HPL.URL_TITLE_READ
-    ).get_attribute("innerText")
+    init_url_row_title = url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).text
 
     url_title = UTS.TEST_URL_TITLE_UPDATE
     update_url_title(browser, url_row, url_title)
@@ -471,9 +474,7 @@ def test_update_url_title_cancel_click_btn(
     assert url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).is_displayed()
 
     # Extract URL string from updated URL row
-    url_row_title = url_row.find_element(
-        By.CSS_SELECTOR, HPL.URL_TITLE_READ
-    ).get_attribute("innerText")
+    url_row_title = url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).text
 
     assert init_url_row_title == url_row_title
 
@@ -504,9 +505,7 @@ def test_update_url_title_cancel_press_escape(
     url_row = get_selected_url(browser)
 
     # Extract URL string from updated URL row
-    init_url_row_title = url_row.find_element(
-        By.CSS_SELECTOR, HPL.URL_TITLE_READ
-    ).get_attribute("innerText")
+    init_url_row_title = url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).text
 
     url_title = UTS.TEST_URL_TITLE_UPDATE
     update_url_title(browser, url_row, url_title)
@@ -518,54 +517,280 @@ def test_update_url_title_cancel_press_escape(
     assert url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).is_displayed()
 
     # Extract URL string from updated URL row
-    url_row_title = url_row.find_element(
-        By.CSS_SELECTOR, HPL.URL_TITLE_READ
-    ).get_attribute("innerText")
+    url_row_title = url_row.find_element(By.CSS_SELECTOR, HPL.URL_TITLE_READ).text
 
     assert init_url_row_title == url_row_title
 
 
-@pytest.mark.skip(
-    reason="Not on happy path. This test tests functionality that is not yet captured on the frontend"
-)
-def test_update_url_title_length_exceeded(browser: WebDriver, create_test_urls):
+def test_update_url_title_length_exceeded(
+    browser: WebDriver,
+    create_test_urls,
+    runner: Tuple[Flask, FlaskCliRunner],
+    provide_app: Flask,
+):
     """
-    GIVEN a user trying to add a new UTub
-    WHEN they submit the addUTub form
-    THEN ensure the appropriate input field is shown and in focus
-    """
+    Tests the site error response to a user's attempt to update a URL with a title that exceeds the maximum character length limit.
 
-    login_utub_url(browser)
-
-    # update_url_title(browser, UTS.MAX_CHAR_LIM_URL_TITLE, MOCK_URL_STRINGS[0])
-
-    warning_modal_body = wait_then_get_element(browser, HPL.BODY_MODAL)
-    assert warning_modal_body is not None
-
-    # Assert new UTub is now active and displayed to user
-    assert warning_modal_body.text == "Try shortening your UTub name"
-
-
-@pytest.mark.skip(
-    reason="Not on happy path. This test tests functionality that is not yet captured on the frontend"
-)
-def test_update_url_string_length_exceeded(browser: WebDriver, create_test_urls):
-    """
-    GIVEN a user trying to add a new UTub
-    WHEN they submit the addUTub form
-    THEN ensure the appropriate input field is shown and in focus
+    GIVEN a user and selected UTub
+    WHEN the updateURL title form is populated and submitted with a title that exceeds character limits
+    THEN ensure the appropriate error and prompt is shown to user.
     """
 
-    login_utub_url(browser)
+    _, cli_runner = runner
+    app = provide_app
+    add_mock_urls(cli_runner, list([UTS.TEST_URL_STRING_CREATE]))
 
-    # update_url_string(browser, MOCK_URL_STRINGS[0], UTS.MAX_CHAR_LIM_URL_STRING)
+    user_id_for_test = 1
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
 
-    warning_modal_body = wait_then_get_element(browser, HPL.BODY_MODAL)
-    assert warning_modal_body is not None
+    url_row = get_selected_url(browser)
 
-    # Assert new UTub is now active and displayed to user
-    assert warning_modal_body.text == "Try shortening your UTub name"
+    update_url_title(browser, url_row, "a" * (URL_CONSTANTS.MAX_URL_TITLE_LENGTH + 1))
+
+    update_url_title_input = wait_then_get_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.INPUT_URL_TITLE_UPDATE}", time=3
+    )
+
+    assert update_url_title_input is not None
+    new_url_title = update_url_title_input.get_attribute("value")
+    assert new_url_title is not None
+    assert len(new_url_title) == URL_CONSTANTS.MAX_URL_TITLE_LENGTH
 
 
-# TODO: Check url title sanitization for sad path tests
-# TODO Check invalid CSRF token for sad path tests
+def test_update_url_string_empty_field(
+    browser: WebDriver, create_test_urls, provide_app
+):
+    """
+    GIVEN a user and selected UTub
+    WHEN the updateURL string form is submitted empty
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
+
+    url_row = get_selected_url(browser)
+    update_url_string(browser, url_row, "")
+
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_STRING_SUBMIT_UPDATE}", time=3
+    )
+
+    invalid_url_string_error = wait_then_get_element(
+        browser, HPL.INPUT_URL_STRING_UPDATE + HPL.INVALID_FIELD_SUFFIX, time=3
+    )
+    assert invalid_url_string_error is not None
+    assert invalid_url_string_error.text == URL_FAILURE.FIELD_REQUIRED_STR
+
+
+def test_update_url_title_empty_field(
+    browser: WebDriver, create_test_urls, provide_app: Flask
+):
+    """
+    GIVEN a user and selected UTub
+    WHEN the updateURL title form is submitted empty
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
+
+    url_row = get_selected_url(browser)
+    update_url_title(browser, url_row, "")
+
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_TITLE_SUBMIT_UPDATE}", time=3
+    )
+
+    invalid_url_title_error = wait_then_get_element(
+        browser, HPL.INPUT_URL_TITLE_UPDATE + HPL.INVALID_FIELD_SUFFIX, time=3
+    )
+    assert invalid_url_title_error is not None
+    assert invalid_url_title_error.text == URL_FAILURE.FIELD_REQUIRED_STR
+
+
+def test_update_url_string_duplicate_url(
+    browser: WebDriver, create_test_urls, provide_app: Flask
+):
+    """
+    GIVEN a user and selected UTub
+    WHEN the updateURL string form is submitted with a URL that is already in the UTub
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+    with app.app_context():
+        utub: Utubs = Utubs.query.filter(Utubs.utub_creator == user_id_for_test).first()
+        utub_url: Utub_Urls = Utub_Urls.query.filter(
+            Utub_Urls.utub_id == utub.id
+        ).first()
+        url_to_update_to: str = utub_url.standalone_url.url_string
+        another_utub_url: Utub_Urls = Utub_Urls.query.filter(
+            Utub_Urls.url_title != utub_url.url_title
+        ).first()
+
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, utub.name, another_utub_url.url_title
+    )
+
+    url_row = get_selected_url(browser)
+    update_url_string(browser, url_row, url_to_update_to)
+
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_STRING_SUBMIT_UPDATE}", time=3
+    )
+
+    error_css_selector = f"{HPL.ROW_SELECTED_URL} {HPL.INPUT_URL_STRING_UPDATE + HPL.INVALID_FIELD_SUFFIX}"
+    wait_until_visible_css_selector(browser, error_css_selector, timeout=3)
+
+    invalid_url_string_error = wait_then_get_element(
+        browser, error_css_selector, time=3
+    )
+    assert invalid_url_string_error is not None
+    assert invalid_url_string_error.text == URL_FAILURE.URL_IN_UTUB
+
+
+def test_update_url_string_invalid_url(
+    browser: WebDriver, create_test_urls, provide_app: Flask
+):
+    """
+    GIVEN a user and selected UTub
+    WHEN the updateURL string form is submitted with an invalid URL
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
+
+    url_row = get_selected_url(browser)
+    update_url_string(browser, url_row, "Test")
+
+    add_invalid_url_header_for_ui_test(browser)
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_STRING_SUBMIT_UPDATE}", time=3
+    )
+
+    error_css_selector = f"{HPL.ROW_SELECTED_URL} {HPL.INPUT_URL_STRING_UPDATE + HPL.INVALID_FIELD_SUFFIX}"
+    wait_until_visible_css_selector(browser, error_css_selector, timeout=3)
+
+    invalid_url_string_error = wait_then_get_element(
+        browser, error_css_selector, time=3
+    )
+    assert invalid_url_string_error is not None
+    assert invalid_url_string_error.text == URL_FAILURE.UNABLE_TO_VALIDATE_THIS_URL
+
+
+def test_update_url_sanitized_title(
+    browser: WebDriver, create_test_urls, provide_app: Flask
+):
+    """
+    Tests the site error response to a user's attempt to update a URL with a title that
+    contains improper or unsanitized inputs
+
+    GIVEN a user and selected UTub
+    WHEN the updateURL title form is submitted with an invalid URL title that is sanitized
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
+
+    url_row = get_selected_url(browser)
+
+    update_url_title(browser, url_row, '<img src="evl.jpg">')
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_TITLE_SUBMIT_UPDATE}", time=3
+    )
+
+    error_css_selector = f"{HPL.ROW_SELECTED_URL} {HPL.INPUT_URL_TITLE_UPDATE + HPL.INVALID_FIELD_SUFFIX}"
+    wait_until_visible_css_selector(browser, error_css_selector, timeout=3)
+
+    invalid_url_title_error = wait_then_get_element(browser, error_css_selector, time=3)
+    assert invalid_url_title_error is not None
+    assert invalid_url_title_error.text == URL_FAILURE.INVALID_INPUT
+
+
+def test_update_url_title_invalid_csrf_token(
+    browser: WebDriver, create_test_urls, provide_app: Flask
+):
+    """
+    Tests the site error response to a user's attempt to update a URL with a title
+    with an invalid CSRF token
+
+    GIVEN a user and selected UTub
+    WHEN the updateURL title form is submitted with an invalid CSRF token
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+    with app.app_context():
+        user: Users = Users.query.get(user_id_for_test)
+
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
+
+    url_row = get_selected_url(browser)
+
+    update_url_title(browser, url_row, "Testing")
+    invalidate_csrf_token_on_page(browser)
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_TITLE_SUBMIT_UPDATE}", time=3
+    )
+
+    assert_visited_403_on_invalid_csrf_and_reload(browser)
+
+    # Page reloads after user clicks button in CSRF 403 error page
+    with pytest.raises(NoSuchElementException):
+        browser.find_element(By.CSS_SELECTOR, HPL.ROW_SELECTED_URL)
+
+    assert_login_with_username(browser, user.username)
+
+
+def test_update_url_string_invalid_csrf_token(
+    browser: WebDriver, create_test_urls, provide_app: Flask
+):
+    """
+    Tests the site error response to a user's attempt to update a URL with a url string
+    with an invalid CSRF token
+
+    GIVEN a user and selected UTub
+    WHEN the updateURL string form is submitted with an invalid CSRF token
+    THEN ensure the appropriate error and prompt is shown to user.
+    """
+    app = provide_app
+    user_id_for_test = 1
+    with app.app_context():
+        user: Users = Users.query.get(user_id_for_test)
+
+    login_user_select_utub_by_name_and_url_by_title(
+        app, browser, user_id_for_test, UTS.TEST_UTUB_NAME_1, UTS.TEST_URL_TITLE_1
+    )
+
+    url_row = get_selected_url(browser)
+
+    update_url_string(browser, url_row, "Testing")
+    invalidate_csrf_token_on_page(browser)
+    wait_then_click_element(
+        browser, f"{HPL.ROW_SELECTED_URL} {HPL.BUTTON_URL_STRING_SUBMIT_UPDATE}", time=3
+    )
+
+    assert_visited_403_on_invalid_csrf_and_reload(browser)
+
+    # Page reloads after user clicks button in CSRF 403 error page
+    with pytest.raises(NoSuchElementException):
+        browser.find_element(By.CSS_SELECTOR, HPL.ROW_SELECTED_URL)
+
+    assert_login_with_username(browser, user.username)
